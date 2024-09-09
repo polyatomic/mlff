588003cf7a11e4188684674a1e871181c5a801ca
Andre Lomaka <andre7lomaka@gmail.com>
2024-09-09 10:07:58 +0300

    Added python wrapper for Mol objects

--- chemutil/module.cpp
+++ chemutil/module.cpp
@@ -7,8 +7,14 @@
 typedef struct {
    PyObject_HEAD
    int size;
-   Iterator<Mol> mols;
+   Iterator<Mol> *mols;
 } MolIteratorObject;
+
+typedef struct {
+   PyObject_HEAD
+   int natoms;
+   Mol *mol;
+} MolObject;
 
 static PyObject* MolIterator_new(PyTypeObject *type, PyObject *args, PyObject *kwds) {
    MolIteratorObject *self;
@@ -17,6 +23,35 @@ static PyObject* MolIterator_new(PyTypeObject *type, PyObject *args, PyObject *k
       self->size = 0;
    }
    return (PyObject*)self;
+}
+
+static int MolIterator_init(MolIteratorObject *self, PyObject *args, PyObject *kwds) {
+   self->mols = new Iterator<Mol>;
+   return 0;
+}
+
+static void MolIterator_dealloc(MolIteratorObject *self) {
+   delete self->mols;
+   Py_TYPE(self)->tp_free((PyObject *)self);
+}
+
+static PyObject* Mol_new(PyTypeObject *type, PyObject *args, PyObject *kwds) {
+   MolObject *self;
+   self = (MolObject*)type->tp_alloc(type, 0);
+   if (self != NULL) {
+      self->natoms = 0;
+   }
+   return (PyObject*)self;
+}
+
+static int Mol_init(MolObject *self, PyObject *args, PyObject *kwds) {
+   self->mol = new Mol;
+   return 0;
+}
+
+static void Mol_dealloc(MolObject *self) {
+   delete self->mol;
+   Py_TYPE(self)->tp_free((PyObject *)self);
 }
 
 static PyObject* MolIterator_Size(MolIteratorObject *self, PyObject *Py_UNUSED(ignored)) {
@@ -28,13 +63,17 @@ static PyObject* MolIterator_ReadXYZFile(MolIteratorObject *self, PyObject *args
    if (!PyArg_ParseTuple(args, "s", &str)) {
       return NULL;
    }
-   self->mols = ReadXYZFile(str);
-   self->size = self->mols.Size();
+   *self->mols = ReadXYZFile(str);
+   self->size = self->mols->Size();
    if (self->size == 0) {
       PyErr_Format(PyExc_Exception, "Error opening %s", str);
       return NULL;
    }
    Py_RETURN_NONE;
+}
+
+static PyObject* Mol_NAtoms(MolObject *self, PyObject *Py_UNUSED(ignored)) {
+   return PyLong_FromLong(self->natoms);
 }
 
 static PyMethodDef MolIterator_methods[] = {
@@ -43,12 +82,21 @@ static PyMethodDef MolIterator_methods[] = {
    { NULL }
 };
 
-static PyTypeObject MolIteratorType = {
+static PyMethodDef Mol_methods[] = {
+   { "NAtoms", (PyCFunction)Mol_NAtoms, METH_NOARGS, "Return the number of atoms" },
+   { NULL }
+};
+
+static Py_ssize_t MolIterator_len(MolIteratorObject *self) {
+   return self->size;
+}
+
+static PyTypeObject MolType = {
    PyVarObject_HEAD_INIT(NULL, 0)            // ob_base
-   "chemutil.MolIterator",                   // tp_name
-   sizeof(MolIteratorObject),                // tp_basicsize
+   "chemutil.Mol",                           // tp_name
+   sizeof(MolObject),                        // tp_basicsize
    0,                                        // tp_itemsize
-   nullptr,                                  // tp_dealloc
+   (destructor)Mol_dealloc,                  // tp_dealloc
    0,                                        // tp_vectorcall_offset
    nullptr,                                  // tp_getattr
    nullptr,                                  // tp_setattr
@@ -57,6 +105,80 @@ static PyTypeObject MolIteratorType = {
    nullptr,                                  // tp_as_number
    nullptr,                                  // tp_as_sequence
    nullptr,                                  // tp_as_mapping
+   nullptr,                                  // tp_hash
+   nullptr,                                  // tp_call
+   nullptr,                                  // tp_str
+   nullptr,                                  // tp_getattro
+   nullptr,                                  // tp_setattro
+   nullptr,                                  // tp_as_buffer
+   Py_TPFLAGS_DEFAULT,                       // tp_flags
+   PyDoc_STR("Mol objects"),                 // tp_doc
+   nullptr,                                  // tp_traverse
+   nullptr,                                  // tp_clear
+   nullptr,                                  // tp_richcompare
+   0,                                        // tp_weaklistoffset
+   nullptr,                                  // tp_iter
+   nullptr,                                  // tp_iternext
+   Mol_methods,                              // tp_methods
+   nullptr,                                  // tp_members
+   nullptr,                                  // tp_getset
+   nullptr,                                  // tp_base
+   nullptr,                                  // tp_dict
+   nullptr,                                  // tp_descr_get
+   nullptr,                                  // tp_descr_set
+   0,                                        // tp_dictoffset
+   (initproc)Mol_init,                       // tp_init
+   nullptr,                                  // tp_alloc
+   Mol_new,                                  // tp_new
+   nullptr,                                  // tp_free
+   nullptr,                                  // tp_is_gc
+   nullptr,                                  // tp_bases
+   nullptr,                                  // tp_mro
+   nullptr,                                  // tp_cache
+   nullptr,                                  // tp_subclasses
+   nullptr,                                  // tp_weaklist
+   nullptr,                                  // tp_del
+   0,                                        // tp_version_tag
+   nullptr,                                  // tp_finalize
+   nullptr                                   // tp_vectorcall
+};
+
+static PyObject* MolIterator_getitem(MolIteratorObject *self, PyObject *ix) {
+   size_t index = PyLong_AsSize_t(ix);
+   if (index >= self->size) {
+      PyErr_SetString(PyExc_IndexError, "Out of bounds!");
+      return nullptr;
+   }
+   //MolObject *mo = PyObject_New(MolObject, &MolType);
+   MolObject *mo = (MolObject *)PyObject_CallNoArgs((PyObject*)&MolType);
+   if (!mo) {
+      return PyErr_NoMemory();
+   }
+   *mo->mol = (*self->mols)[index];
+   mo->natoms = mo->mol->NAtoms();
+   return (PyObject*)mo;
+}
+
+static PyMappingMethods MolIterator_mappings = {
+   (lenfunc)MolIterator_len,
+   (binaryfunc)MolIterator_getitem,
+   nullptr
+};
+
+static PyTypeObject MolIteratorType = {
+   PyVarObject_HEAD_INIT(NULL, 0)            // ob_base
+   "chemutil.MolIterator",                   // tp_name
+   sizeof(MolIteratorObject),                // tp_basicsize
+   0,                                        // tp_itemsize
+   (destructor)MolIterator_dealloc,          // tp_dealloc
+   0,                                        // tp_vectorcall_offset
+   nullptr,                                  // tp_getattr
+   nullptr,                                  // tp_setattr
+   nullptr,                                  // tp_as_async
+   nullptr,                                  // tp_repr
+   nullptr,                                  // tp_as_number
+   nullptr,                                  // tp_as_sequence
+   &MolIterator_mappings,                    // tp_as_mapping
    nullptr,                                  // tp_hash
    nullptr,                                  // tp_call
    nullptr,                                  // tp_str
@@ -79,7 +201,7 @@ static PyTypeObject MolIteratorType = {
    nullptr,                                  // tp_descr_get
    nullptr,                                  // tp_descr_set
    0,                                        // tp_dictoffset
-   nullptr,                                  // tp_init
+   (initproc)MolIterator_init,               // tp_init
    nullptr,                                  // tp_alloc
    MolIterator_new,                          // tp_new
    nullptr,                                  // tp_free
@@ -112,6 +234,9 @@ PyMODINIT_FUNC PyInit_chemutil() {
    if (PyType_Ready(&MolIteratorType) < 0)
       return NULL;
 
+   if (PyType_Ready(&MolType) < 0)
+      return NULL;
+
    m = PyModule_Create(&chemutilmodule);
    if (m == NULL)
       return NULL;
@@ -119,6 +244,13 @@ PyMODINIT_FUNC PyInit_chemutil() {
    Py_INCREF(&MolIteratorType);
    if (PyModule_AddObject(m, "MolIterator", (PyObject*)&MolIteratorType) < 0) {
       Py_DECREF(&MolIteratorType);
+      Py_DECREF(m);
+      return NULL;
+   }
+
+   Py_INCREF(&MolType);
+   if (PyModule_AddObject(m, "Mol", (PyObject*)&MolType) < 0) {
+      Py_DECREF(&MolType);
       Py_DECREF(m);
       return NULL;
    }
--- py_wrapper_test/py_wrapper_test.py
+++ py_wrapper_test/py_wrapper_test.py
@@ -3,3 +3,6 @@ import chemutil
 mols = chemutil.MolIterator()
 mols.ReadXYZFile('benzene.xyz')
 print('File contains {} geometries'.format(mols.Size()))
+if mols.Size() > 0:
+   mol = mols[0]
+   print('Molecule contains {} atoms'.format(mol.NAtoms()))
